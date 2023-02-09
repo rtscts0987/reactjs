533004c271b03eccd6d226d393333eebe0e149c4
Hyunkuk Kim <60034879+rtscts0987@users.noreply.github.com>
2023-02-09 20:29:14 +0900

    0.7.0

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { useState, useEffect} from "react";
+// import { useState, useEffect} from "react";
+import { useState} from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("create :)");
-    return () => console.log("destory =]")
-  }, [])
-  return <h1>hello</h1>;
-}
 
 function App() { 
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
-  
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value)
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray])
+    setToDo("");
+  }
   return (
     <div >
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button onClick={onSubmit}>Add To Do</button>
+      </form>
     </div>
   );
 }
